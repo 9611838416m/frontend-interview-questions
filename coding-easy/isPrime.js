1d136eb418013fd3cf0f01819d665b8905b1cbb5
Alex Zhou <zhou48@indiana.edu>
2017-07-21 17:56:05 -0700

    Fix isPrime function for number 2
    2 is a prime number, so isPrime function need to return true when n === 2. (Suggested by Yuhang Alex Zhou)

--- coding-easy/isPrime.js
+++ coding-easy/isPrime.js
@@ -13,6 +13,9 @@ function isPrime(n) {
   if (n < 2) {
     return false
   }
+  if (n === 2) {
+    return true
+  }
   for (let i = 2; i < Math.ceil(Math.sqrt(n)) + 1; i++) {
     if (n % i === 0) {
       return false
